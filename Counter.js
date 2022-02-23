dc688e53c1416d6c48cb98b19c18fd0be827ed72
Subramaniam Jeyaseelan <86115120+jeyaseelan6@users.noreply.github.com>
2022-02-24 00:37:43 +0530

    added src files

---
+++ Counter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+class Counter extends React.Component{
+    state = {
+      name : "Jeyaseelan",
+      age : 21
+    }
+  
+    increment = ()=>{
+      this.setState({
+          name :this.state.name + 'S',
+        age: this.state.age + 5});
+      }
+  
+      render(){
+        return <>
+          <p>{this.state.name}</p>
+          <p>{this.state.age}</p>
+          <button onClick={this.increment}>Increment</button>
+        </>
+      
+      }
+    }
+
+    
+export default Counter;
